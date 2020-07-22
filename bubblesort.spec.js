986831c00206e178ffe5a742ffd9a47669cafe44
Britt Preyer <britt_preyer@britts-mac-mini.lan>
2020-07-22 12:37:02 -0400

    handing off workign on the bubble sort recursion

---
+++ bubblesort.spec.js
@@ -0,0 +1,22 @@
+describe("Bubble Sort", function () {
+  it("handles and empty array", function () {
+    expect(bubbleSort([])).toEqual([]);
+  });
+  it("handles single item array", function () {
+    expect(bubbleSort([1])).toEqual([1]);
+  });
+  it("handles multiple item array", function () {
+    expect(bubbleSort([8, 2, 5, 100, 3000, 7, 1, 6, 10, 3])).toEqual([
+      1,
+      2,
+      3,
+      5,
+      6,
+      7,
+      8,
+      10,
+      100,
+      3000,
+    ]);
+  });
+});
