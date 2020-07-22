ae60b3025b315e487a8d8fd9ce4f8b01b7939f4c
Gregory Millare <gmillare4@gmail.com>
2020-07-22 11:50:17 -0500

    added recursion

--- bubblesort.js
+++ bubblesort.js
@@ -1,23 +1,18 @@
-function bubbleSort(array) {
+function bubbleSort(array, counter = array.length - 1) {
   if (array.length === 1 || array.length === 0) return array;
-  let counter = array.length;
-  counter--;
-  if (counter > 0) {
-    return bubbleSort(array);
-  }
-  if (counter === 0) {
-    return array;
-  }
 
-  for (let i = 0; i < array.length - 1; i++) {
+  for (let i = 0; i < counter; i++) {
     let j = i + 1;
-    if (array[i] < array[j]) {
-      continue;
-    } else if (array[i] > array[j]) {
+    if (array[i] > array[j]) {
       let moveElem = array.splice(i, 1);
       array.splice(j, 0, moveElem[0]);
     }
   }
+  counter--;
+  if (counter !== 0) {
+    bubbleSort(array, counter);
+  }
+  return array;
 }
 
 // [8, 2, 5, 7]
